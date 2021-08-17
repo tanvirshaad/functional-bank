c51190d78c1ed07c42437b0b92ae6d09cd4a9949
tanvirshaad <tanvirshaad@gmail.com>
2021-08-17 18:11:53 +0600

    deposit and balance completed

---
+++ js/banking.js
@@ -0,0 +1,17 @@
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const depositAmountText = depositInput.value;
+    const depositAmount = parseFloat(depositAmountText);
+    //get current deposit
+    const depositTotal = document.getElementById('deposit-total');
+    const depositTotalText = depositTotal.innerText;
+    const previousDepositTotal = parseFloat(depositTotalText);
+    depositTotal.innerText = depositAmount + previousDepositTotal;
+    //update balance
+    const balanceTotal = document.getElementById('balance-total');
+    const balanceTotalText = balanceTotal.innerText;
+    const previousBalanceTotal = parseFloat(balanceTotalText);
+    balanceTotal.innerText = previousBalanceTotal + depositAmount;
+    //clear the input field
+    depositInput.value = '';
+});
